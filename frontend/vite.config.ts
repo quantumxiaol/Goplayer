d46c831965debdfaa40723e062175512ca3f94d0
quantumxiaol <liuxiaolong.0425@foxmail.com>
2026-04-21 12:02:53 +0800

    fix Goplayer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/goplayer/',
+  base: '/Goplayer/',
   build: {
     outDir: 'dist',
   },
